3b45c1cd4447421ba19b284c0aaf7fdb322609c2
kellysweeney021 <86845524+kellysweeney021@users.noreply.github.com>
2022-03-16 09:20:26 -0500

    remove rows that don't have all values

---
+++ removeRows.py
@@ -0,0 +1,56 @@
+import json
+import requests
+import csv
+import pandas as pd
+
+# get active fires in cali
+url = 'https://www.fire.ca.gov/umbraco/api/IncidentApi/List'
+r = requests.get(url)
+output = r.json()
+
+# get earthquakes within 1000km of active fires in cali
+countUrl = 'https://earthquake.usgs.gov/fdsnws/event/1/count'
+
+result = []
+for obj in output:
+    latitude = str(obj['Latitude'])
+    longitude = str(obj['Longitude'])
+    params = {'format': 'geojson', 'eventtype': 'earthquake', 'latitude': obj['Latitude'], 'longitude': obj['Longitude'], 'maxradiuskm': '1000', 'orderby': 'time-asc'}
+    # get earthquakes within 1000km api call
+    r2 = requests.get(countUrl, params=params)
+    countOutput = r2.json()
+    earthquakes = str(countOutput['count'])
+    # get grid points to later find elevation
+    pointUrl = 'https://api.weather.gov/points/' + latitude + ',' + longitude
+    # api call for lat/long to get elevation
+    r3 = requests.get(pointUrl)
+    pointsOutput = r3.json()
+    gridId = str(pointsOutput['properties']['gridId'])
+    gridX = str(pointsOutput['properties']['gridX'])
+    gridY = str(pointsOutput['properties']['gridY'])
+    # get lat/long's elevation - final api call
+    elevationUrl = 'https://api.weather.gov/gridpoints/' + gridId + '/' + gridX + ',' + gridY
+    r4 = requests.get(elevationUrl)
+    elevationOutput = r4.json()
+    altitude = str(elevationOutput['properties']['elevation']['value'])
+
+    fireRows = [latitude,longitude,earthquakes,altitude]
+    # set results from looping through as an array to output to csv later
+    result.append(fireRows)
+
+# output to csv
+# header = ['latitude', 'longitude', '# of earthquakes in 1000km', 'elevation in m']
+
+# with open('/Users/kellysweeney/apis/backstopFinal.csv', 'w') as csv_file:
+#     writer = csv.writer(csv_file)
+#     writer.writerow(header)
+#     writer.writerows(result)
+
+df = pd.DataFrame(result)
+# df=df[header]
+df = df[df[3] != '0']
+df.columns = ["latitude", "longitude", "# of earthquakes in 1000km", "elevation in m"]
+df.to_csv('/Users/kellysweeney/apis/finalFinal.csv', index=False)
+# print(df)
+
+exit()
